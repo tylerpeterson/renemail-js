5a5512a55a11f3db18bbc83790168ba961e16346
Tyler Peterson <stronglytyped@gmail.com>
2022-03-18 18:51:03 -0600

    Actually move the file. be careful about path. read enough for forwards.

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
 #! /usr/bin/env node
 
 import { program } from 'commander'
-import { readFile } from 'fs/promises'
+import { readFile, rename } from 'fs/promises'
 import { parse, format } from 'date-fns'
+import { join, dirname, extname } from 'path'
 
 async function renemail () {
   program
     .description('Rename an email file based on its contents.')
   program.parse()
 
-  const filePromises = program.args.map(filename => readFile(filename, { encoding: 'utf8' }))
-  filePromises.forEach(async function (filePromise) {
+  const filenames = program.args
+  const filePromises = filenames.map(filename => readFile(filename, { encoding: 'utf8' }))
+  const renamePromises = []
+  filePromises.forEach(async function (filePromise, i) {
     const file = await filePromise
-    const head = file.substring(0, 2048)
+    const filename = filenames[i]
+    const head = file.substring(0, 4096)
     const foundDate = head.match(/^Date: \w+, (.*)$/m)[1]
     const date = parse(foundDate, 'd LLL yyyy HH:mm:ss XXXX', new Date())
     const fileDateTime = format(date, 'yyyy-MM-dd-HH-mm')
     const foundSubject = head.match(/^Subject: (.*)$/m)[1]
     const simpleSubject = foundSubject.replace(/\W+/g, '_').substring(0, 65)
+    const ext = extname(filename)
+    const newName = `${fileDateTime} ${simpleSubject}${ext}`
+    const newPath = join(dirname(filename), newName)
+    renamePromises.push(rename(filename, newPath))
 
-    console.log(`${fileDateTime}--${simpleSubject}`)
+    console.log(`mv ${filename} ${newPath}`)
   })
 }
 
