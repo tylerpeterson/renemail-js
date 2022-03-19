4c91ea7b45919bc01a51b5a2d62aad24b75d1a30
Tyler Peterson <stronglytyped@gmail.com>
2022-03-18 18:28:52 -0600

    only scan the top of the email for date and subject

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { program } from 'commander'
 import { readFile } from 'fs/promises'
 import { parse, format } from 'date-fns'
 
-async function renemail() {
+async function renemail () {
   program
     .description('Rename an email file based on its contents.')
   program.parse()
@@ -12,10 +12,11 @@ async function renemail() {
   const filePromises = program.args.map(filename => readFile(filename, { encoding: 'utf8' }))
   filePromises.forEach(async function (filePromise) {
     const file = await filePromise
-    const foundDate = file.match(/^Date: \w+, (.*)$/m)[1]
+    const head = file.substring(0, 2048)
+    const foundDate = head.match(/^Date: \w+, (.*)$/m)[1]
     const date = parse(foundDate, 'd LLL yyyy HH:mm:ss XXXX', new Date())
     const fileDateTime = format(date, 'yyyy-MM-dd-HH-mm')
-    const foundSubject = file.match(/^Subject: (.*)$/m)[1]
+    const foundSubject = head.match(/^Subject: (.*)$/m)[1]
     const simpleSubject = foundSubject.replace(/\W+/g, '_').substring(0, 65)
 
     console.log(`${fileDateTime}--${simpleSubject}`)
