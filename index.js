627383e4b9eefe5ad7c914f392727f50e1c76adf
Tyler Peterson <petersontb@familysearch.org>
2023-04-05 09:59:06 -0600

    FIX bad files stop all processing
    Log failed files and continue
    
    Capture the filename being processed
    Try to process file
    Catch all errors and log them

--- index.js
+++ index.js
@@ -18,19 +18,23 @@ async function renemail () {
   const filePromises = filenames.map(filename => readFile(filename, { encoding: 'utf8' }))
   const renamePromises = []
   filePromises.forEach(async function (filePromise, i) {
-    const file = await filePromise
     const filename = filenames[i]
-    const foundDate = file.match(/^Date: (?:\w+, )?(.*\d{4})( \(\w+\))?$/m)[1]
-    const date = parse(foundDate, 'd LLL yyyy HH:mm:ss XXXX', new Date())
-    const fileDateTime = format(date, 'yyyy-MM-dd-HH-mm')
-    const foundSubject = file.match(/^Subject: (.*)$/m)[1]
-    const simpleSubject = foundSubject.replace(/\W+/g, '_').substring(0, 65)
-    const ext = extname(filename)
-    const newName = `${fileDateTime} ${simpleSubject}${ext}`
-    const newPath = join(dirname(filename), newName)
-    renamePromises.push(rename(filename, newPath))
-
-    console.log(`mv ${filename} ${newPath}`)
+    try {
+      const file = await filePromise
+      const foundDate = file.match(/^Date: (?:\w+, )?(.*\d{4})( \(\w+\))?$/m)[1]
+      const date = parse(foundDate, 'd LLL yyyy HH:mm:ss XXXX', new Date())
+      const fileDateTime = format(date, 'yyyy-MM-dd-HH-mm')
+      const foundSubject = file.match(/^Subject: (.*)$/m)[1]
+      const simpleSubject = foundSubject.replace(/\W+/g, '_').substring(0, 65)
+      const ext = extname(filename)
+      const newName = `${fileDateTime} ${simpleSubject}${ext}`
+      const newPath = join(dirname(filename), newName)
+      renamePromises.push(rename(filename, newPath))
+  
+      console.log(`mv ${filename} ${newPath}`)
+    } catch (err) {
+      console.error(`ERR processing ${filename}.\n${err.name}: ${err.message}`)
+    }
   })
 }
 
